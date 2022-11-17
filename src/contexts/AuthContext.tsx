5cfaf3e0845bae9c0c821a4361723804fc669a78
Italo Alves <italo_silva007@hotmail.com>
2022-11-17 20:02:38 -0300

    feat: add authentication context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,23 +1,69 @@
-import {
-  createContext,
-  Dispatch,
-  ReactNode,
-  SetStateAction,
-  useState,
-} from 'react'
+import Router from 'next/router'
+import { destroyCookie, parseCookies, setCookie } from 'nookies'
+import { createContext, ReactNode, useEffect, useState } from 'react'
+import { api } from '../services/api'
+
+interface SignInCredentials {
+  email: string
+  password: string
+}
+
+interface User {
+  accessToken: string
+}
 
 interface AuthContextData {
+  signIn({ email, password }: SignInCredentials): Promise<void>
   isAuthenticated: boolean
-  setIsAuthenticated: Dispatch<SetStateAction<boolean>>
+  user: User | null
 }
 
 export const AuthContext = createContext({} as AuthContextData)
 
+export function signOut() {
+  destroyCookie(undefined, 'cliniflyToken', {
+    path: '/',
+  })
+  Router.push('/')
+}
+
 export function AuthProvider({ children }: { children: ReactNode }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [user, setUser] = useState<User | null>(null)
+  const isAuthenticated = !!user
+
+  useEffect(() => {
+    const cookies = parseCookies()
+    if (!cookies.cliniflyToken) {
+      setUser(null)
+    } else {
+      setUser({ accessToken: cookies.cliniflyToken })
+    }
+  }, [])
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post('eventos', {
+        type: 'patientAuth',
+        data: { email, password },
+      })
+
+      const { accessToken } = response.data
+
+      setCookie(undefined, 'cliniflyToken', accessToken, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: '/',
+      })
+
+      setUser({ accessToken })
+
+      Router.push('/home')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+    <AuthContext.Provider value={{ isAuthenticated, user, signIn }}>
       {children}
     </AuthContext.Provider>
   )
